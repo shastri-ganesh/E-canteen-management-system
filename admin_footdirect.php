9d5955d0751619790ae2e1dddd3d7c2d2552461a
Ganesh Shastri <120932885+shastri-ganesh@users.noreply.github.com>
2025-05-17 12:39:58 +0530

    Add files via upload

--- admin_footdirect.php
+++ admin_footdirect.php
@@ -6,17 +6,24 @@
     <meta charset="UTF-8">
      
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/login.css" rel="stylesheet">
     <link href="../img/Color Icon with background.png" rel="icon">
-    <link href="../css/login.css" rel="stylesheet">
 
-    <title>Database Error | FOODCAVE</title>
+    <title>About Us | FOODCAVE</title>
+    <style>
+       html,body {
+  height:100%;
+  min-height:100%;
+  max-height:100%;
+}
+    </style>
 </head>
 
 <body class="d-flex flex-column h-100">
     <header class="navbar navbar-expand-md navbar-light fixed-top bg-light shadow-sm mb-auto">
         <div class="container-fluid mx-4">
-            <a href="../index.php">
-                <img src="../img/LOGO_BLACK.png" width="125" class="me-2" alt=" FOODCAVE Logo">
+            <a href="index.php">
+                <img src="../img/Color logo - no background.png" width="125" class="me-2" alt="FOODCAVE Logo">
             </a>
             <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
@@ -30,10 +37,10 @@
     </header>
     <div class="mt-5"></div>
     <div class="container form-signin text-center restricted mt-auto">
-            <i class="mt-4 bi bi-hdd-network-fill text-danger h1 display-2"></i>
-            <h3 class="mt-2 mb-3 fw-normal text-bold">Connection Error</h3>
-            <p class="mb-3 fw-normal text-bold text-wrap">Cannot connect to FOODCAVE database right now.</p>
-            <a class="btn btn-danger btn-sm w-50" href="../index.php">Try again.</a>
+            <i class="mt-4 bi bi-telephone-fill text-danger h1 display-2"></i>
+            <h3 class="mt-2 mb-3 fw-normal text-bold">About Us</h3>
+            <p class="mb-3 fw-normal text-bold text-wrap">The Alpha Achievers</p>
+            <a class="btn btn-danger btn-sm w-50" href="admin_home.php">Return To Home</a>
     </div>
 
     <?php include('admin_footer.php')?>
